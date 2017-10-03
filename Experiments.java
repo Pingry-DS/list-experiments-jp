bd4b99faf8a8291ae6b0ce8dc45b8475183f8cf6
Jack Proudfoot <jproudfoot2018@pingry.org>
2017-10-03 15:17:14 -0400

    CPU Timing
    CPU timing using threading

--- Experiments.java
+++ Experiments.java
@@ -1,49 +1,60 @@
 import java.util.List;
 import java.util.ArrayList;
+import java.util.LinkedList;
+
+import java.lang.Thread;
+import java.lang.management.ThreadMXBean;
+import java.lang.management.ManagementFactory;
 
 public class Experiments {
 
   public static void main(String[] args) {
-  
+
     //TODO Check command line for number of iterations
     int iterations = 10000;
-    
+
+	//Need this to setup CPU timing
+	ThreadMXBean bean = ManagementFactory.getThreadMXBean();
+
     // Keep track of the run time for each call
-    long start = System.nanoTime();
+    long start = bean.getThreadCpuTime(Thread.currentThread().getId());
     long end;
-    
+	
     // Make the testing calls and print the time after each
     HeadInsert(iterations, "Hello");
-    end = System.nanoTime();
+	end = bean.getThreadCpuTime(Thread.currentThread().getId());
+	
+	
     System.out.println("Insertion at head took " + (end - start)/1000000.0 + "ms.\n");
     
+
     start = end;
-    TailInsert(iterations, "Hello");
-    end = System.nanoTime();
+	TailInsert(iterations, "Hello");
+	end = bean.getThreadCpuTime(Thread.currentThread().getId());
     System.out.println("Insertion at tail took " + (end - start)/1000000.0 + "ms.\n");
-    
+	
     start = end;
     MidpointInsert(iterations, "Hello");
-    end = System.nanoTime();
+    end = bean.getThreadCpuTime(Thread.currentThread().getId());
     System.out.println("Insertion at midpoint took " + (end - start)/1000000.0 + "ms.\n");
-    
+
     start = end;
     AlternateInsert(iterations, "Hello");
-    end = System.nanoTime();
+    end = bean.getThreadCpuTime(Thread.currentThread().getId());
     System.out.println("Insertion at tail took " + (end - start)/1000000.0 + "ms.\n");
-    
+
     start = end;
     AlternateInsert(iterations, "Hello");
-    end = System.nanoTime();
+    end = bean.getThreadCpuTime(Thread.currentThread().getId());
     System.out.println("Alternate insertion took " + (end - start)/1000000.0 + "ms.\n");
-    
+
     start = end;
     SortedInsert(iterations);
-    end = System.nanoTime();
+    end = bean.getThreadCpuTime(Thread.currentThread().getId());
     System.out.println("Sorted insertion took " + (end - start)/1000000.0 + "ms.\n");
-  
+
   }
-  
+
   /**
    * Creates a List and inserts the given payload the specified number of times at the head of the list
    * bumping all previous entries down the List.
@@ -53,9 +64,13 @@ public class Experiments {
    * @return A reference to the constructed List
    */
   public static List<String> HeadInsert(int times, String payload) {
-  
+    LinkedList<String> list = new LinkedList<String>();
+    for (int x = 0; x < times; x++) {
+      list.addFirst(payload);
+    }
+    return list;
   }
-  
+
   /**
    * Creates a List and inserts the given payload the specified number of times at the tail.
    *
@@ -64,10 +79,14 @@ public class Experiments {
    * @return A reference to the constructed List
    */
   public static List<String> TailInsert(int times, String payload) {
-  
+    ArrayList<String> list = new ArrayList<String>();
+    for (int x = 0; x < times; x++) {
+      list.add(payload);
+    }
+    return list;
   }
-  
-  
+
+
   /**
    * Creates a List and inserts the given payload the specified number of times in the middle of the list
    * bumping previous entries down the List as necessary. When calculating the midpoint, round down to the
@@ -79,13 +98,13 @@ public class Experiments {
    * @return A reference to the constructed List
    */
   public static List<String> MidpointInsert(int times, String payload) {
-    List<T> l = new ArrayList<T>();
+    List<String> l = new ArrayList<String>();
     for(int i = 0; i < times; i++) {
       l.add(l.size() / 2, payload);
     }
     return l;
   }
-  
+
   //TODO Maybe make a ReverseAlternateInsert too.
   /**
    * Creates a List and inserts the given payload the specified number of times as if the List
@@ -96,9 +115,9 @@ public class Experiments {
    * @return A reference to the constructed List
    */
   public static List<String> AlternateInsert(int times, String payload) {
-  
+    return null;
   }
-  
+
   //TODO Use a comparator in this method
   /**
    * Creates a List and inserts the given payload items, in the order they are given. Each item
@@ -108,7 +127,7 @@ public class Experiments {
    * @param items The items to be inserted. Given in no particular order.
    * @return A reference to the constructed List
    */
-  public static List<String> SortedInsert(List<String> items) {
+  public static List<String> SortedInsert(int i) {
     return null;
   }
 
